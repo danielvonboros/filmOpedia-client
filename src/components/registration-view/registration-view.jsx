08b4620f9730eb06d33ae5861648c879f3cbf30d
danielvonboros <dboros40@gmail.com>
2021-07-14 17:07:04 +0200

    update registration-view
    src/components/registration-view/registration-view
    - update registration-view, input validation, back-button functionality

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -6,7 +6,7 @@ import Col from "react-bootstrap/Col";
 import axios from "axios";
 import axiosInstance from "../../config/";
 
-import Redirect from "react-router-dom";
+import Redirect, { Link } from "react-router-dom";
 
 import "./registration-view.scss";
 
@@ -16,15 +16,77 @@ export default function RegistrationView(props) {
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
 
+  const [validateUser, setValidateUser] = useState("");
+  const [validatePassword, setValidatePassword] = useState("");
+  const [validateEmail, setValidateEmail] = useState("");
+  const [validateBirthday, setValidateBirthday] = useState("");
   // const handleSubmit = (e) => {
   //     e.preventDefault();
   //     console.log(username, password. email, birthday);
   //     props.onRegister(username);
   // };
 
+  const validateUsername = (e) => {
+    if (e.target.value.length > 0 && e.target.length < 3) {
+      setValidateUser("Username must be longer than 3 characters.");
+    } else {
+      setValidateUser("");
+    }
+    if (
+      !e.currentTarget.value.match(/^[0-9a-zA-Z]+$/) &&
+      e.target.value.length > 0
+    ) {
+      setValidateUser("Only alphanumeric characters are allowed!");
+    }
+  };
+
+  const validatePasswordInput = (e) => {
+    if (e.target.value.length > 0 && e.target.value.length < 8) {
+      setValidatePassword("Password has to be at least 8 characters long");
+    } else {
+      setValidatePassword("");
+    }
+  };
+
+  const validateEmailInput = (e) => {
+    if (e.target.value.length > 0 && !e.target.value.match(/\S+@\S+\.\S+/)) {
+      setValidateEmail("Invalid Email");
+    } else {
+      setValidateEmail("");
+    }
+  };
+
+  const validateBirthdayInput = (e) => {
+    if (
+      e.target.value.length > 0 &&
+      e.target.value.match(/^\d{2}-\d{2}-\d{4}$/)
+    ) {
+      setValidateBirthday(
+        "Invalid date format, please use the format DD-MM-YYYY"
+      );
+    } else {
+      setValidateBirthday("");
+    }
+  };
+
   const handleRegister = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
+    if (
+      newUsername.length === 0 ||
+      newPassword.length === 0 ||
+      newEmail.length === 0 ||
+      newBirthday.length === 0
+    ) {
+      alert("Please fill all the fields");
+      return false;
+    }
+
+    if (validateUser || validatePassword || validateEmail || validateBirthday) {
+      alert("Incorrect input");
+      return false;
+    }
+
     axiosInstance
       .post("/users", {
         username: username,
@@ -51,45 +113,53 @@ export default function RegistrationView(props) {
           <Form.Control
             type="text"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => {
+              setUsername(e.target.value), validateUsername(e);
+            }}
           />
+          <span className="validation-feedback">{validateUser}</span>
         </Form.Group>
         <Form.Group>
           <Form.Label controlid="password">Password: </Form.Label>
           <Form.Control
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value), validatePasswordInput(e);
+            }}
           />
+          <span className="validation-feedback">{validatePassword}</span>
         </Form.Group>
         <Form.Group>
           <Form.Label controlid="email">eMail: </Form.Label>
           <Form.Control
             type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              setEmail(e.target.value), validateEmailInput(e);
+            }}
           />
+          <span className="validation-feedback">{validateEmail}</span>
         </Form.Group>
         <Form.Group>
           <Form.Label controlid="birthday">Date of Birth: </Form.Label>
           <Form.Control
             type="date"
             value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
+            onChange={(e) => {
+              setBirthday(e.target.value), validateBirthdayInput(e);
+            }}
           />
+          <span className="validation-feedback">{validateBirthday}</span>
         </Form.Group>
         <Button variant="danger" type="submit" onClick={handleRegister}>
           Submit
         </Button>{" "}
-        <Button
-          variant="outline-secondary"
-          className="button-float-right"
-          onClick={() => {
-            onBackClick(null);
-          }}
-        >
-          Back
-        </Button>
+        <Link to={`/`}>
+          <Button variant="outline-secondary" className="button-float-right">
+            Back
+          </Button>
+        </Link>
       </Col>
     </Row>
   );
